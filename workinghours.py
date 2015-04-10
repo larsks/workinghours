5a087fc1dd25bae57d00975bc158d29b06d934d7
Lars Kellogg-Stedman <lars@redhat.com>
2015-04-10 11:37:32 -0400

    added --drift option

--- workinghours.py
+++ workinghours.py
@@ -29,6 +29,9 @@ def parse_args():
                    nargs=2)
     p.add_argument('--repo', '-r',
                    default='.')
+    p.add_argument('--drift', '-d',
+                   nargs=3,
+                   default=(0.5,2,2))
     p.add_argument('revspec', nargs='?', default='HEAD')
     p.set_defaults(interval='workday')
     return p.parse_args()
@@ -106,9 +109,20 @@ def main():
         # our modified commits are still ordered correctly w/r/t time) and
         # reverse the list, since we are getting commits from
         # batched_commits in latest-to-earliest order.
-        offsets = sorted([random.uniform(0, delta.total_seconds())
-                          for x in range(len(batch))],
-                         reverse=True)
+        if random.random() < float(args.drift[0]):
+            offset_left = random.randint(0, int(args.drift[1])*60*60)
+        else:
+            offset_left = 0
+
+        if random.random() < float(args.drift[0]):
+            offset_right = random.randint(0, int(args.drift[2])*60*60)
+        else:
+            offset_right = 0
+
+        offsets = sorted((
+            random.uniform(-offset_left,
+                           delta.total_seconds() + offset_right)
+            for x in range(len(batch))), reverse=True)
 
         for i, commit in enumerate(batch):
             when = time_start + datetime.timedelta(seconds=offsets[i])
